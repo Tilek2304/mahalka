8a198f7a30c6cca4a8b56f3f592acc14760728d9
Tilek2304 <esenaliev2304@gmail.com>
2025-04-27 19:24:35 +0500

    123

--- robot_control/views.py
+++ robot_control/views.py
@@ -2,7 +2,13 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .motor_control import MotorController
 
-motor_ctl = MotorController()  # Ваш класс управления моторами
+motor_ctl = MotorController(
+            pca,
+            left_ch_a=pca.channels[1],  # IN1
+            left_ch_b=pca.channels[0],  # IN2
+            right_ch_a=pca.channels[3], # IN3
+            right_ch_b=pca.channels[2] # IN4
+        )
 
 @csrf_exempt
 def control_command(request):
